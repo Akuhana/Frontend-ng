c745db2c864314a828088a1a83711c96125c0a41
Akuhana <nikarka1@gmail.com>
2025-05-10 23:07:12 +0200

    init

---
+++ src/app/app.config.ts
@@ -0,0 +1,37 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
+
+import { routes } from './app.routes';
+
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    // override the font-family for body/text
+    font: {
+      family: "'Inter', sans-serif"
+    }
+  }
+});
+
+providePrimeNG({
+  theme: {
+    preset: MyPreset
+  }
+});
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+            theme: {
+                preset: MyPreset
+            }
+        }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)]
+};
